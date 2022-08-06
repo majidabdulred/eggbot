0f8f042ad9d830eabfa2d19c795a890b75fe9826
Abdul Majid <abdulmajidred@gmail.com>
2022-08-06 23:47:11 +0530

    configured api

--- apis/chickenderby.py
+++ apis/chickenderby.py
@@ -1,58 +1,51 @@
 from aiohttp import request
 from util.logs import mylogs
 
+from models.model_races_scheduled import ScheduledRaces
+from models.model_races_results import RaceResults
+from models.model_races_by_chicken import RacesByChicken
+from models.model_get_chicken import get_chickens_model
 API_URL = "https://api.chickenderby.com"
 
 
 async def get_scheduled_races():
-    URL = API_URL + "/api/race/all-races-scheduled"
-    async with request("GET", url=URL) as res:
+    URL = API_URL + '/api/races?page=1&limit=100&filter={"status":"scheduled"}'
+    async with request(method="GET", url=URL) as res:
         if res.status != 200:
             mylogs.error(f"CHICKENDERBY_API_ERROR : {URL} : {await res.text()}")
             raise ValueError
         result = await res.json()
-        return result
+        return ScheduledRaces(result)
 
 
 async def get_race_results():
-    URL = API_URL + "/api/race/game-results?page=1&limit=3"
+    URL = API_URL + '/api/results?page=1&limit=10'
     async with request("GET", url=URL) as res:
         if res.status != 200:
             mylogs.error(f"CHICKENDERBY_RACE_RESULTS : {URL} : {await res.text()}")
             raise ValueError
         results = await res.json()
-        return results
+        return RaceResults(results)
 
 
-async def get_chickens(chicks: list) -> list:
+async def get_chickens(chicks: list):
     URL = "https://crun-minter.herokuapp.com/tokens"
     async with request("POST", URL, json={"tokenIds": chicks}) as res:
         if res.status != 200:
             mylogs.error(f"CRUN_API_ERROR : {URL} : {await res.text()}")
             raise ValueError
         results = await res.json()
-        return results["body"]
+        return get_chickens_model(results)
 
 
 async def get_chicken_race_history(chickenid, page, limit):
-    URL = f"https://api.chickenderby.com/api/race/get-races-by-chicken?chickenId={chickenid}&page={page}&limit={limit}"
-    async with request("GET",url=URL) as res:
+    URL = API_URL+f"/api/races/chickens/{chickenid}?page={page}&limit={limit}"
+    async with request("GET", url=URL) as res:
         if res.status != 200:
             mylogs.error(f"CHICKEN_RACE_HISTORY_API_ERROR : {URL} : {res.text()}")
             raise ValueError
         results = await res.json()
-        return results["rows"],results["count"]
-
-
-async def get_weth_price():
-    URL = "https://api.chickenderby.com/api/getCoinRate"
-    async with request("GET", URL) as res:
-        if res.status != 200:
-            mylogs.error(f"GET_COIN_RATE_ERROR : {URL} : {await res.text()}")
-            raise ValueError
-        results = await res.json()
-        price = results.get("weth")
-        return price
+        return RacesByChicken(results)
 
 
 async def get_race_data(raceid):
